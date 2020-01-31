491248ffb532e2f5384dbf29a95e64cf23b8ff56
Alex Mills <alexanderwmills@gmail.com>
2020-01-31 12:13:58 +0000

    Updates

---
+++ precache-manifest.f42811caa1cd7a42098007898b421360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9394375d8e9ce8a8032ba746a1678c8e",
+    "url": "/raisely/index.html"
+  },
+  {
+    "revision": "f5a4c2c9b59c3bdd0cb5",
+    "url": "/raisely/static/css/main.a979051d.chunk.css"
+  },
+  {
+    "revision": "e0332d98c529c480deb4",
+    "url": "/raisely/static/js/2.bc9cbe30.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/raisely/static/js/2.bc9cbe30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5a4c2c9b59c3bdd0cb5",
+    "url": "/raisely/static/js/main.5d3bc30b.chunk.js"
+  },
+  {
+    "revision": "a8e7d6cd1a93baac8be8",
+    "url": "/raisely/static/js/runtime-main.21a725cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raisely/static/media/logo.5d5d9eef.svg"
+  }
+]);
